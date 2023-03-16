d402b6a5c949b6ffd88dca09f166b42585ec4444
Daphne <daphne@Daphnes-MacBook-Pro.local>
2023-03-16 15:09:28 +0800

    questions and answers done

--- flutter_quiz/lib/answer.dart
+++ flutter_quiz/lib/answer.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-        child: Text('Answer 1'),
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
--- flutter_quiz/lib/main.dart
+++ flutter_quiz/lib/main.dart
@@ -14,22 +14,32 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
     // method provided by State that takes a function that changes property of code
-    setState(() {      
-    _questionIndex = _questionIndex + 1;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?',
+    const questions = const [
+      {
+        'questionText': 'Pick a color',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'Pick an animal',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      },
+      {
+        'questionText': 'Who is your favorite person?',
+        'answers': ['Daph', 'Daph', 'Daph', 'Daph'],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -39,16 +49,16 @@ class _MyAppState extends State<MyApp>{
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            // spread operator takes a list and pulls all values inside so we don't have a list within a list, but values within a list (not nested list)
+            // tell dart answers is a list of strings
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
     );
   }
 }
-
-
